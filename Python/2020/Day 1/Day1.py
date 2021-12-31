7d4aa270092ed270571f7b81998dd8c9b7ccebdd
Bortugini <63115967+Bortugini@users.noreply.github.com>
2021-12-31 01:08:09 +0100

    clean up

--- Python/2020/Day 1/Day1.py
+++ Python/2020/Day 1/Day1.py
@@ -10,8 +10,26 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 with open(ROOT_DIR + "/numbers") as file:
     numbers = list(map(int, file.readlines()))
 
+numbers = [1721, 979, 366, 299, 675, 1456]
+
+for x in numbers:
+    for y in numbers:
+        if x + y == 2020:
+            print(x * y)
+            break
+    else:
+        continue
+    break
+
 for x in numbers:
     for y in numbers:
         for z in numbers:
             if x+y+z == 2020:
                 print(x*y*z)
+                break
+        else:
+            continue
+        break
+    else:
+        continue
+    break
