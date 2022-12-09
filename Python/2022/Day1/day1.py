a2fa5b1408fb9e1148f14863f78da51bfbe8e0dd
Bortugini <63115967+Bortugini@users.noreply.github.com>
2022-12-09 20:40:45 +0100

    add day 1

--- Python/2022/Day1/day1.py
+++ Python/2022/Day1/day1.py
@@ -8,14 +8,21 @@ import os
 elfs = []
 elf = []
 calories = 0
+
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 with open(ROOT_DIR + "/calories") as file:
-    for line in file:
-        if line in '\n':
-            print("Leer")
-            calories = sum(elf)
-            elfs.append(calories)
-        else:
-            elf.append(int(line))
+    lines = file.readlines()
 
-print(elfs)
+for i in range(len(lines)):
+    if lines[i] in '\n' or i == len(lines) - 1:
+        if i == len(lines) - 1:
+            elf.append(int(lines[i]))
+        calories = sum(elf)
+        elf.clear()
+        elfs.append(calories)
+    else:
+        elf.append(int(lines[i]))
+
+
+print("Part 1 " + str(max(elfs)))
+print("Part 2 " + str(sum(sorted(elfs)[-3:])))
